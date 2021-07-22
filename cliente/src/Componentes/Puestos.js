0aab42ee2abfb71dfe448f7a703c9e6941774725
Maikmm30 <maikmm30@gmail.com>
2021-07-22 17:07:51 -0600

    Se agrega eliminar a puestos

--- cliente/src/Componentes/Puestos.js
+++ cliente/src/Componentes/Puestos.js
@@ -2,11 +2,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import BootstrapTable from 'react-bootstrap-table-next';
 import cellEditFactory from 'react-bootstrap-table2-editor';
 import {
-    Link
-  } from "react-router-dom";
-  import Axios from "axios";
-  import React, { useState, useEffect } from "react";
-  
+  Link
+} from "react-router-dom";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+
 function Puestos() {
 
   const [puestos, setPuesto] = useState([]);
@@ -17,7 +17,7 @@ function Puestos() {
   var [puestoNuevo, setNuevoPuesto] = useState("");
   var [columnaSeleccionada, setColumna] = useState("");
 
-    
+
   useEffect(() => {
     Axios.get("http://localhost:3001/puestos/").then((res) => {
       setPuesto(res.data);
@@ -35,33 +35,33 @@ function Puestos() {
   }
 
   const buscar = () => {
-    Axios.post("http://localhost:3001/puestos/buscar", 
-    {
-      codigoBusca : codigoBusca,
-      nombreBusca : nombreBusca
-    })
-    .then((res) => {
-      setPuesto(res.data);
-    });
+    Axios.post("http://localhost:3001/puestos/buscar",
+      {
+        codigoBusca: codigoBusca,
+        nombreBusca: nombreBusca
+      })
+      .then((res) => {
+        setPuesto(res.data);
+      });
   };
-  
-    
-const capturaInput = (event) => {
-  if (!event.target.value == '') {
-    setNuevoPuesto(event.target.value)
-    console.log(event.target)
-  }
-};
 
 
-const capturaBusca = ()=>{
-  if(codigoBusca && nombreBusca!== ''){
-    buscar()
+  const capturaInput = (event) => {
+    if (!event.target.value == '') {
+      setNuevoPuesto(event.target.value)
+      console.log(event.target)
+    }
+  };
+
+
+  const capturaBusca = () => {
+    if (codigoBusca && nombreBusca !== '') {
+      buscar()
+    }
+    else {
+      alert('Por favor ingrese los datos')
+    }
   }
-  else{
-   alert('Por favor ingrese los datos')
-  }
-}
 
   const columns = [
     {
@@ -108,38 +108,59 @@ const capturaBusca = ()=>{
 
   const rowEvents = {
     onClick: (e, row, rowIndex) => {
-      setCodigoActualiza(JSON.parse(row.codigo))
+      setCodigoActualiza(row.codigo)
     }
   };
 
   const recarga = () => {
     window.location.reload();
   };
-  
+
   const limpiaCajas = () => {
     setCodigo("");
     setNombre("");
   };
-    
+
+  const capturaEliminar = () => {
+    if (codigoBusca !== '') {
+      eliminarDato()
+    }
+    else {
+      alert('Por favor ingrese el codigo')
+    }
+  }
+
+  const eliminarDato = () => {
+    Axios.put("http://localhost:3001/puestos/eliminar",
+      {
+        codigoBusca: codigoBusca
+      })
+      .then(() => {
+
+        window.location.reload()
+      });
+
+  };
+
 
 
   return (
     <div class="container">
-      <div class="row " style={{ height: "800px" , backgroundColor: "#FF723F"  }}>
+      <div class="row " style={{ height: "800px", backgroundColor: "#FF723F" }}>
         <div class="col-3 m-auto text-center pb-5">
           <h3>Lista de Puestos</h3>
           <i class="fas fa-user-friends fa-10x"></i>
         </div>
         <div class="col-9">
           <div class="row h-75">
-            <div class="text-center col-12  h-25" style={{  backgroundColor: "#C42709"}}>
-                <div class="row row-cols-4 m-4">
-               
-                  <div class="col"><button class=" p-3  rounded-circle fas fa-broom fa-3x "  onClick={limpiaCajas}></button></div>
-                
-                  <div class="col "><button class="p-3  rounded-circle  fas fa-check-circle fa-3x"  onClick={capturaBusca}></button></div>
-                  <div class="col"><button class=" py-3 px-4  rounded-circle fas fa-times fa-3x"></button></div>
-                  <div class="col"><button class=" py-3 px-4  rounded-circle fas fa-sync fa-3x"   onClick={recarga}></button></div>
+            <div class="text-center col-12  h-25" style={{ backgroundColor: "#C42709" }}>
+              <div class="row row-cols-4 m-4">
+
+                <div class="col"><button class=" p-3  rounded-circle fas fa-broom fa-3x " onClick={limpiaCajas}></button></div>
+
+                <div class="col "><button class="p-3  rounded-circle  fas fa-check-circle fa-3x" onClick={capturaBusca}></button></div>
+                <div class="col"><button class=" py-3 px-4  rounded-circle fas fa-times fa-3x"></button></div>
+                <div class="col"><button class=" py-3 px-4  rounded-circle fas fa-sync fa-3x" onClick={recarga}></button></div>
 
               </div>
             </div>
@@ -150,52 +171,55 @@ const capturaBusca = ()=>{
                   Código del puesto
                 </label>
                 <div class="col-sm-4">
-                      <input
-                        type="number"
-                        class="form-control"
-                        value={codigoBusca}
-                        onChange={(event) => {
-                          setCodigo(event.target.value);
-                        }}
-                      />
-                    </div>
-                    <label  class="col-sm-2 col-form-label">
+                  <input
+                    type="number"
+                    class="form-control"
+                    value={codigoBusca}
+                    onChange={(event) => {
+                      setCodigo(event.target.value);
+                    }}
+                  />
+                </div>
+                <label class="col-sm-2 col-form-label">
                   Nombre del puesto
                 </label>
                 <div class="col-sm-4">
-                      <input
-                        type="text"
-                        class="form-control"
-                        value={nombreBusca}
-                        onChange={(event) => {
-                          setNombre(event.target.value);
-                        }}
-                      />
-                    </div>
-              </div>
-              
-              <div class="form-group row mt-2">
-                
-                <div class="py-5 px-5"
-                   onKeyUp={capturaInput} onBlur={actualizaPais}>
-                <BootstrapTable
-                    keyField="id"
-                    data={ puestos }
-                    columns={ columns }
-                    rowEvents={rowEvents}
-                    cellEdit={ cellEditFactory({ mode: 'dbclick' }) }
-                />
+                  <input
+                    type="text"
+                    class="form-control"
+                    value={nombreBusca}
+                    onChange={(event) => {
+                      setNombre(event.target.value);
+                    }}
+                  />
                 </div>
-                
-              <div class="text-center col-12 h-25" style={{  backgroundColor: "#C42709"}}>
-                <div class="row row-cols-2 m-4">
-                <Link to='/agregarPuestos'> 
-                  <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-plus-circle fa-3x"></button></div>
-                </Link>
-                  <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-minus-circle fa-3x"></button></div>
-
               </div>
-            </div>
+
+              <div class="form-group row mt-2">
+
+                <div class="py-5 px-5"
+                  onKeyUp={capturaInput} onBlur={actualizaPais}>
+                  <BootstrapTable
+                    keyField="id"
+                    data={puestos}
+                    columns={columns}
+                    rowEvents={rowEvents}
+                    cellEdit={cellEditFactory({ mode: 'dbclick' })}
+                  />
+                </div>
+
+                <div class="text-center col-12 h-25" style={{ backgroundColor: "#C42709" }}>
+                  <div class="row row-cols-2 m-4">
+                    <Link to='/agregarPuestos'>
+                      <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-plus-circle fa-3x"></button></div>
+                    </Link>
+                    
+                    <div className="col">
+                      <button className=" py-3 px-4 bg-light rounded-circle fas fa-minus-circle fa-3x" onClick={capturaEliminar}></button>
+                    </div>
+
+                  </div>
+                </div>
               </div>
             </div>
           </div>

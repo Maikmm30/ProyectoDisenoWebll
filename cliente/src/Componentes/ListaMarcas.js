cf52d69bc0eeb95fe66f5021790884a73a29cd79
Maikmm30 <maikmm30@gmail.com>
2021-07-23 12:46:35 -0600

    Se agrega eliminar a Marca

--- cliente/src/Componentes/ListaMarcas.js
+++ cliente/src/Componentes/ListaMarcas.js
@@ -4,13 +4,13 @@ import cellEditFactory from 'react-bootstrap-table2-editor';
 import Axios from "axios";
 import React, { useState, useEffect } from "react";
 import {
-    Link
-  } from "react-router-dom";
+  Link
+} from "react-router-dom";
 
-  
+
 function ListaMarcas() {
 
-  
+
   const [marcas, setMarca] = useState([]);
   var [codigoBusca, setCodigo] = useState("");
   var [nombreBusca, setNombre] = useState("");
@@ -36,14 +36,14 @@ function ListaMarcas() {
   }
 
   const buscar = () => {
-    Axios.post("http://localhost:3001/marcas/buscar", 
-    {
-      codigoBusca : codigoBusca,
-      nombreBusca : nombreBusca
-    })
-    .then((res) => {
-      setMarca(res.data);
-    });
+    Axios.post("http://localhost:3001/marcas/buscar",
+      {
+        codigoBusca: codigoBusca,
+        nombreBusca: nombreBusca
+      })
+      .then((res) => {
+        setMarca(res.data);
+      });
   };
 
   const capturaInput = (event) => {
@@ -51,16 +51,16 @@ function ListaMarcas() {
       setNuevoMarca(event.target.value)
     }
   };
-  
-  
-const capturaBusca = ()=>{
-  if(codigoBusca && nombreBusca!== ''){
-    buscar()
+
+
+  const capturaBusca = () => {
+    if (codigoBusca && nombreBusca !== '') {
+      buscar()
+    }
+    else {
+      alert('Por favor ingrese los datos')
+    }
   }
-  else{
-   alert('Por favor ingrese los datos')
-  }
-}
 
   const columns = [
     {
@@ -124,23 +124,44 @@ const capturaBusca = ()=>{
       }
     },
   ];
-  
+
   const rowEvents = {
     onClick: (e, row, rowIndex) => {
-      setCodigoActualiza(JSON.parse(row.codigo))
+      setCodigoActualiza(row.codigo)
     }
   };
 
-  
+
   const recarga = () => {
     window.location.reload();
   };
-  
+
   const limpiaCajas = () => {
     setCodigo("");
     setNombre("");
   };
-    
+
+  const capturaEliminar = () => {
+    if (codigoBusca !== '') {
+      eliminarDato()
+    }
+    else {
+      alert('Por favor ingrese el codigo')
+    }
+  }
+
+  const eliminarDato = () => {
+    Axios.put("http://localhost:3001/marcas/eliminar",
+      {
+        codigoBusca: codigoBusca
+      })
+      .then(() => {
+
+        window.location.reload()
+      });
+
+  };
+
 
   return (
     <div class="container">
@@ -152,13 +173,13 @@ const capturaBusca = ()=>{
         <div class="col-9">
           <div class="row h-75">
             <div class="text-center col-12 bg-success h-25">
-                <div class="row row-cols-4 m-4">
-                
-                  <div class="col"><button class=" p-3 bg-light rounded-circle fas fa-broom fa-3x "  onClick={limpiaCajas}></button></div>
-                  
-                  <div class="col "><button class="p-3 bg-light rounded-circle  fas fa-check-circle fa-3x" onClick={capturaBusca}></button></div>
-                  <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-times fa-3x"></button></div>
-                  <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-sync fa-3x"  onClick={recarga}></button></div>
+              <div class="row row-cols-4 m-4">
+
+                <div class="col"><button class=" p-3 bg-light rounded-circle fas fa-broom fa-3x " onClick={limpiaCajas}></button></div>
+
+                <div class="col "><button class="p-3 bg-light rounded-circle  fas fa-check-circle fa-3x" onClick={capturaBusca}></button></div>
+                <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-times fa-3x"></button></div>
+                <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-sync fa-3x" onClick={recarga}></button></div>
 
               </div>
             </div>
@@ -169,51 +190,53 @@ const capturaBusca = ()=>{
                   Código de la Marca
                 </label>
                 <div class="col-sm-4">
-                      <input
-                        type="number"
-                        class="form-control"
-                        value={codigoBusca}
-                        onChange={(event) => {
-                          setCodigo(event.target.value);
-                        }}
-                      />
-                    </div>
-                    <label for="staticEmail" class="col-sm-2 col-form-label">
+                  <input
+                    type="number"
+                    class="form-control"
+                    value={codigoBusca}
+                    onChange={(event) => {
+                      setCodigo(event.target.value);
+                    }}
+                  />
+                </div>
+                <label for="staticEmail" class="col-sm-2 col-form-label">
                   Nombre de la Marca
                 </label>
                 <div class="col-sm-4">
-                      <input
-                        type="text"
-                        class="form-control"
-                        value={nombreBusca}
-                        onChange={(event) => {
-                          setNombre(event.target.value);
-                        }}
-                      />
-                    </div>
-              </div>
-              
-              <div class="form-group row mt-2">
-                
-                <div class="col-sm-12"
-                   onKeyUp={capturaInput} onBlur={actualizaPais}>
-                <BootstrapTable
-                    keyField="id"
-                    data={ marcas }
-                    columns={ columns }
-                    rowEvents={rowEvents}
-                    cellEdit={ cellEditFactory({ mode: 'dbclick' }) }
-                />
+                  <input
+                    type="text"
+                    class="form-control"
+                    value={nombreBusca}
+                    onChange={(event) => {
+                      setNombre(event.target.value);
+                    }}
+                  />
                 </div>
-                
-              <div class="text-center col-12 bg-success h-25">
-                <div class="row row-cols-2 m-4">
-                <Link to='/marcas'> 
-                  <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-plus-circle fa-3x"></button></div>
-                  </Link>
-                  <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-minus-circle fa-3x"></button></div>
               </div>
-            </div>
+
+              <div class="form-group row mt-2">
+
+                <div class="col-sm-12"
+                  onKeyUp={capturaInput} onBlur={actualizaPais}>
+                  <BootstrapTable
+                    keyField="id"
+                    data={marcas}
+                    columns={columns}
+                    rowEvents={rowEvents}
+                    cellEdit={cellEditFactory({ mode: 'dbclick' })}
+                  />
+                </div>
+
+                <div class="text-center col-12 bg-success h-25">
+                  <div class="row row-cols-2 m-4">
+                    <Link to='/marcas'>
+                      <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-plus-circle fa-3x"></button></div>
+                    </Link>
+                    <div className="col">
+                      <button className=" py-3 px-4 bg-light rounded-circle fas fa-minus-circle fa-3x" onClick={capturaEliminar}></button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>

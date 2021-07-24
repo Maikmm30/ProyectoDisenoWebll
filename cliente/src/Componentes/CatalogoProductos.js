ebaed49c9d4ce4534a422793e507997619d035f2
scamposr020 <scamposr020@ulacit.ed.cr>
2021-07-23 23:55:04 -0600

    se agrega estilo a catalogo productos

--- cliente/src/Componentes/CatalogoProductos.js
+++ cliente/src/Componentes/CatalogoProductos.js
@@ -2,98 +2,78 @@ import React from "react";
 import {
   Link
 } from "react-router-dom";
+import { Card, CardGroup } from "react-bootstrap";
+
 
 function CatalogoProductos(){
-        return (
-            <div class="container">
-      <div class="row ">
-        <div class="col-sm bg-info text-center">
-            <h3>Catálogo de Productos</h3>
-            <i class="fab fa-product-hunt fa-10x text-success"></i>
-        </div>
-        <div class="col-sm bg-warning d-flex align-items-center">
-          <div class="container ">
-            <div class="row ">
-              <div class="col-sm">
-                <div className="form-check">
-                
-                  <label class="form-check-label">Comestibles</label>
-                  <Link to='/comestibles'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                  <label class="form-check-label">Desechos y Empaques</label>
-                  <Link to='/empaques'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                  <label class="form-check-label">Limpieza e Higiene</label>
-                  <Link to='/limpieza'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                </div>
-              </div>
-              <div class="col-sm">
-                <div className="form-check">
-              
-                    <label class="form-check-label">Tecnología</label>
-                    <Link to='/tecnologia'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                    <label class="form-check-label">Equipos y Utensilios</label>
-                    <Link to='/utensilio'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                    </div>
-              </div>
-            </div>
+        return (     <div>
+
+          <div style={{ padding: "30px 350px", backgroundColor: "#FF723F" }}>
+              <h4 className='text-center' style={{ color: "#FFF" }}>Ventana Productos</h4>
+
+              <CardGroup>
+
+                  <Card className="m-5 text-center" >
+                      <Link to='/comestibles'>
+                          <Card.Body>
+                              <i class="fas fa-drumstick-bite fa-5x" style={{ color: "#C42709" }}></i>
+                              <Card.Title >Comestibles</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+
+                  <Card className="m-5 text-center">
+                      <Link to='/empaques'>
+                          <Card.Body>
+                              <i class="fas fa-box fa-5x " style={{ color: "#C42709" }}></i>
+                              <Card.Title>Desechos y Empaques</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+                  
+              </CardGroup>
+
+         
+              <CardGroup >
+
+              <Card className="m-5 text-center ">
+                      <Link to='/limpieza'>
+                          <Card.Body>
+                              <i class="fas fa-broom fa-5x" style={{ color: "#C42709" }}></i>
+                              <Card.Title>Limpieza e Higiene</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+                  
+              <Card className="m-5 text-center ">
+              <Link to='/tecnologia'>
+                  <Card.Body>
+                      <i class="fas fa-laptop fa-5x" style={{ color: "#C42709" }}></i>
+                      <Card.Title>Tecnología</Card.Title>
+                  </Card.Body>
+              </Link>
+          </Card>
+
+              </CardGroup>
+
+              <CardGroup >
+
+              <Card className="m-5 text-center ">
+                      <Link to='/utensilio'>
+                          <Card.Body>
+                              <i class="fas fa-utensils fa-5x" style={{ color: "#C42709" }}></i>
+                              <Card.Title>Equipos y Utensilios</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+                  
+              </CardGroup>
           </div>
-        </div>
-        <div class="col-sm bg-danger text-center">
-          <div class="col-sm bg-success mt-4">
-            <div class="col-sm">
-              <button
-                style={{ border: "none", backgroundColor: "transparent" }}
-              >
-                <i class="p-3 text-light rounded-circle  fas fa-check-circle fa-3x"></i>
-              </button>
-            </div>
-          </div>
-          <div class="col-sm bg-success">
-            <div class="col-sm">
-              <button
-                style={{ border: "none", backgroundColor: "transparent" }}
-              >
-                <i class="p-3 text-light rounded-circle  fas fa-times-circle fa-3x"></i>
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
         );
 };
 

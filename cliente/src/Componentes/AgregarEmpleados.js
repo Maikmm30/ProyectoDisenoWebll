12c21fc662848fb31c26cf8aa8e91da341ad80c1
scamposr020 <scamposr020@ulacit.ed.cr>
2021-07-23 20:37:45 -0600

    Se hace agregar de empleados

--- cliente/src/Componentes/AgregarEmpleados.js
+++ cliente/src/Componentes/AgregarEmpleados.js
@@ -1,4 +1,87 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
 function AgregarEmpleados() {
+
+  const [codigoEmpleado, setCodigoEmpleado] = useState("");
+  const [cedulaEmpleado, setCedulaEmpleado] = useState("");
+  const [numeroEmpleado, setNumeroEmpleado] = useState("");
+  const [nombreEmpleado, setNombreEmpleado] = useState("");
+  const [primerApellidoEmpleado, setPrimerApellidoEmpleado] = useState("");
+  const [segundoApellidoEmpleado, setSegundoApellidoEmpleado] = useState("");
+  const [telefono1Empleado, setTelefono1Empleado] = useState("");
+  const [telefono2Empleado, setTelefono2Empleado] = useState("");
+  const [puestoEmpleado, setPuestoEmpleado] = useState("");
+  const [nacionalidadEmpleado, setNacionalidadEmpleado] = useState("");
+  const [restauranteEmpleado, setRestauranteEmpleado ] = useState("");
+
+  
+  useEffect(() => {
+    Axios.get("http://localhost:3001/empleados/id").then((res) => {
+      const num = parseInt(res.data[0].valorConsecutivo) + 1;
+      setNumeroEmpleado(num);
+
+      const str = "E";
+      setCodigoEmpleado(str + num);
+     
+      Axios.get("http://localhost:3001/restaurantes/names").then((res) => {
+        console.log('data' + res.data)
+        console.log(res.data[1]);
+        var array = [];
+        for (var k in res.data) {
+          console.log(array.push(res.data[k].nombre));
+        }
+        for (var i in array) {
+          document.getElementById("restaurante").innerHTML += "<option value='" + array[i] + "'>" + array[i] + "</option>";
+
+        }
+      });
+   
+    });
+  
+    Axios.get("http://localhost:3001/paises/names").then((res) => {
+      console.log('data' + res.data)
+      console.log(res.data[1]);
+      var array1 = [];
+      for (var k in res.data) {
+        console.log(array1.push(res.data[k].nombre));
+      }
+      for (var i in array1) {
+        document.getElementById("nacionalidad").innerHTML += "<option value='" + array1[i] + "'>" + array1[i] + "</option>";
+
+      }
+    });
+
+  }, []);
+
+  
+  const enviarDatos = () => {
+    Axios.post("http://localhost:3001/empleados/agregar",{
+      codigoEmpleado: codigoEmpleado,
+      cedulaEmpleado: cedulaEmpleado,
+      numeroEmpleado: numeroEmpleado,
+      nombreEmpleado: nombreEmpleado,
+      primerApellidoEmpleado: primerApellidoEmpleado,
+      segundoApellidoEmpleado: segundoApellidoEmpleado,
+      telefono1Empleado: telefono1Empleado,
+      telefono2Empleado: telefono2Empleado,
+      puestoEmpleado: puestoEmpleado,
+      nacionalidadEmpleado: nacionalidadEmpleado,
+      restauranteEmpleado: restauranteEmpleado,
+      estadoEmpleado: true,
+    });
+    Axios.put("http://localhost:3001/consecutivos/update",
+      {
+        codigoActualiza: '10',
+        consecutivoNuevo: numeroEmpleado,
+        columnaSeleccionada: 'valorConsecutivo'
+      });
+    window.location.href = 'http://localhost:3000/empleados'
+  };
+
+
     return (
       <div className="container">
         <div className="row " style={{ height: "800px", backgroundColor: "#FF723F" }}>
@@ -14,7 +97,7 @@ function AgregarEmpleados() {
                     <i className=" p-3  rounded-circle fas fa-broom fa-3x "></i>
                   </div>
                   <div className="col ">
-                    <i className="p-3  rounded-circle  fas fa-check-circle fa-3x"></i>
+                    <i className="p-3  rounded-circle  fas fa-check-circle fa-3x" onClick={enviarDatos}></i>
                   </div>
                   <div className="col">
                     <i className=" py-3 px-4  rounded-circle fas fa-times fa-3x"></i>
@@ -31,35 +114,48 @@ function AgregarEmpleados() {
                       <h4 className="my-4">Información del Empleado</h4>
                       <label className="col-sm-3">Código </label>
                       <div className="col-sm-9">
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control"
+                        readonly="readonly" value={codigoEmpleado} />
                       </div>
                     </div>
   
                     <div className="mt-2 mb-3 row">
                       <label className="col-sm-3">Cédula de identidad</label>
                       <div className="col-sm-9">
-                        <input type="text" className="form-control" />
+                        <input type="number" className="form-control"
+                        onChange={(event)=>{
+                          setCedulaEmpleado(event.target.value);
+                        }}/>
                       </div>
                     </div>
   
                     <div className="mt-2  mb-3 row">
                       <label className="col-sm-3">Nombre</label>
                       <div className="col-sm-9">
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" 
+                        onChange={(event)=>{
+                          setNombreEmpleado(event.target.value);
+                        }}/>
                       </div>
                     </div>
   
                     <div className="mt-2 mb-3 row">
                       <label className="col-sm-3">Primer apellido</label>
                       <div className="col-sm-9">
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control"
+                        onChange={(event)=>{
+                          setPrimerApellidoEmpleado(event.target.value);
+                        }} />
                       </div>
                     </div>
   
                     <div className="mt-2 mb-3 row">
                       <label className="col-sm-3">Segundo apellido</label>
                       <div className="col-sm-9">
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" 
+                        onChange={(event)=>{
+                          setSegundoApellidoEmpleado(event.target.value);
+                        }}/>
                       </div>
                     </div>
   
@@ -67,13 +163,19 @@ function AgregarEmpleados() {
                       <h5>Teléfonos</h5>
                       <div className="col-sm-4">
                         <label>Oficina</label>
-                        <input type="text" className="form-control" />
+                        <input type="number" className="form-control"
+                        onChange={(event)=>{
+                          setTelefono1Empleado(event.target.value);
+                        }} />
                       </div>
   
   
                       <div className="col-sm-4">
                         <label>Celular</label>
-                        <input type="text" className="form-control" />
+                        <input type="number" className="form-control" 
+                        onChange={(event)=>{
+                          setTelefono2Empleado(event.target.value);
+                        }}/>
                       </div>
                     </div>
                     <div class="form-group row mt-2">
@@ -84,6 +186,9 @@ function AgregarEmpleados() {
                 <select
                         class="form-control"
                         id="exampleFormControlSelect1"
+                        onChange={(event)=>{
+                          setPuestoEmpleado(event.target.value);
+                        }}
                       >
                         <option>Puesto 1</option>
                         <option>Puesto 2</option>
@@ -95,17 +200,40 @@ function AgregarEmpleados() {
                 </div>
               </div>
               <div class="form-group row mt-2">
+              <label for="staticEmail" class="col-sm-3 col-form-label">
+                Nacionalidad
+              </label>
+              <div class="col-sm-8">
+              <select
+                      class="form-control"
+                      id="nacionalidad"
+                      onChange={(event)=>{
+                        setNacionalidadEmpleado(event.target.value);
+                      }}
+                      onClick={(event)=>{
+                        setNacionalidadEmpleado(event.target.value);
+                      }}
+                    >
+                      
+                    </select>
+              </div>
+            </div>
+              <div class="form-group row mt-2">
                 <label for="staticEmail" class="col-sm-3 col-form-label">
                   Restaurante
                 </label>
                 <div class="col-sm-8">
                 <select
                         class="form-control"
-                        id="exampleFormControlSelect1"
+                        id="restaurante"
+                        onChange={(event)=>{
+                          setRestauranteEmpleado(event.target.value);
+                        }}
+                        onClick={(event)=>{
+                          setRestauranteEmpleado(event.target.value);
+                        }}
                       >
-                        <option>Restaurante 1</option>
-                        <option>Restaurante 2</option>
-                        <option>Restaurante 3</option>
+                        
                       </select>
                 </div>
               </div>
--- servidor/rutas/empleadosRuta.js
+++ servidor/rutas/empleadosRuta.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 let Empleado = require("../modelos/Empleados");
+let Consecutivo = require("../modelos/Consecutivos");
 const express = require("express");
 const app = express();
 
@@ -10,7 +11,49 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/id").get((req, res) => {
+  
+  Consecutivo.find({nombre: 'empleado'}).select('valorConsecutivo')
+      .then((consecutivo) => res.json(consecutivo))
+      .catch((err) => res.status(400).json("Error: " + err));
+});
 
+router.post("/agregar", async (req, res) => {
+  const codigoEmpleado = req.body.codigoEmpleado;
+  const cedulaEmpleado = req.body.cedulaEmpleado;
+  const numeroEmpleado = req.body.numeroEmpleado;
+  const nombreEmpleado = req.body.nombreEmpleado;
+  const primerApellidoEmpleado = req.body.primerApellidoEmpleado;
+  const segundoApellidoEmpleado = req.body.segundoApellidoEmpleado;
+  const telefono1Empleado = req.body.telefono1Empleado;
+  const telefono2Empleado = req.body.telefono2Empleado;
+  const puestoEmpleado = req.body.puestoEmpleado;
+  const nacionalidadEmpleado = req.body.nacionalidadEmpleado;
+  const restauranteEmpleado = req.body.restauranteEmpleado;
+  const estadoEmpleado= req.body.estadoEmpleado;
+
+  try {
+      const empleado = new Empleado({
+          codigo: codigoEmpleado,
+          cedula: cedulaEmpleado,
+          numero: numeroEmpleado,
+          nombre: nombreEmpleado,
+          primerApellido: primerApellidoEmpleado,
+          segundoApellido: segundoApellidoEmpleado,
+          telefono1:telefono1Empleado,
+          telefono2:telefono2Empleado,
+          puesto: puestoEmpleado,
+          nacionalidad: nacionalidadEmpleado,
+          restaurante: restauranteEmpleado,
+          estado: estadoEmpleado
+      });
+      await empleado.save();
+      res.send("inserted data");
+  } catch (err) {
+      console.log(err);
+      res.status(400).send(error)
+  }
+});
 router.put("/update", async (req, res) => {
   const codigoActualiza = req.body.codigoActualiza;
   const empleadoNuevo = req.body.empleadoNuevo;

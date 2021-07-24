e6ff8d252bcbd01884e3c2c01bd6f07620a66ee5
scamposr020 <scamposr020@ulacit.ed.cr>
2021-07-24 00:24:58 -0600

    se agrega estilo a empaques

--- cliente/src/Componentes/Empaques.js
+++ cliente/src/Componentes/Empaques.js
@@ -143,14 +143,14 @@ function Empaques() {
 
   return (
     <div class="container">
-      <div class="row bg-warning" style={{ height: "800px" }}>
+      <div class="row " style={{ height: "800px" , backgroundColor: "#FF723F" }}>
         <div class="col-3 m-auto text-center pb-5">
           <h3>Lista de Empaques</h3>
-          <i class="fas fa-box fa-10x"></i>
+          <i class="fas fa-box fa-10x text-light"></i>
         </div>
         <div class="col-9">
           <div class="row h-75">
-            <div class="text-center col-12 bg-success h-25">
+            <div class="text-center col-12  h-25" style={{ backgroundColor: "#C42709" }}>
               <div class="row row-cols-4 m-4">
 
                 <div class="col"><button class=" p-3 bg-light rounded-circle fas fa-broom fa-3x " onClick={limpiaCajas}></button></div>
@@ -161,7 +161,7 @@ function Empaques() {
 
               </div>
             </div>
-            <div class="col-12 bg-danger h-80">
+            <div class="col-12 h-80">
               Búsqueda de Empaques
               <div class="form-group row mt-2">
                 <label class="col-sm-2 col-form-label">
@@ -194,7 +194,7 @@ function Empaques() {
 
               <div class="form-group row mt-2">
 
-                <div class="col-sm-12"
+                <div class="col-sm-12 table-scroll"
                   onKeyUp={capturaInput} onBlur={actualizaPais}>
                   <BootstrapTable
                     keyField="id"
@@ -205,7 +205,7 @@ function Empaques() {
                   />
                 </div>
 
-                <div class="text-center col-12 bg-success h-25">
+                <div class="text-center col-12 h-25" style={{ backgroundColor: "#C42709" }}>
                   <div class="row row-cols-2 m-4">
                     <Link to='/agregarEmpaques'>
                       <div class="col"><button class=" py-3 px-4 bg-light rounded-circle fas fa-plus-circle fa-3x"></button></div>

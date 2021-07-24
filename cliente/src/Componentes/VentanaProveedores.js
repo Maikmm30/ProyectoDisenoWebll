06934b86b7b2896dabb79b69e8b0b7a67a2e9351
scamposr020 <scamposr020@ulacit.ed.cr>
2021-07-23 23:29:09 -0600

    se agrega estilo a ventana proveedores

--- cliente/src/Componentes/VentanaProveedores.js
+++ cliente/src/Componentes/VentanaProveedores.js
@@ -2,89 +2,55 @@ import React from "react";
 import {
     Link
   } from "react-router-dom";
+  import { Card, CardGroup } from "react-bootstrap";
 
 function VentanaProveedores(){
         return (
-          <div class="container">
-            <div class="row ">
-              <div class="col-sm bg-info text-center">
-                  <h3>Ventana de Proveedores</h3>
-                  <i class="fas fa-truck fa-10x text-success"></i>
-              </div>
-              <div class="col-sm bg-warning d-flex align-items-center">
-                <div class="container ">
-                  <div class="row ">
-                    <div class="col-sm">
-                        Opciones
-                      <div className="form-check">
+          <div>
 
-                        <label class="form-check-label">Marcas</label>
-                        <Link to='/listaMarcas'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                        <label class="form-check-label"></label>
-                        <Link to='/listaMarcas'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                        <div class="row"></div>
-                        <label class="form-check-label">Productos</label>
-                        <Link to='/catalogoProductos'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                        <label class="form-check-label">Proveedores</label>
-                        <Link to='/Proveedores'>
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            value=""
-                            id=""
-                          />
-                        </Link>
-                      </div>
-                    </div>
-                    <div class="col-sm">
-                      
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm bg-danger text-center">
-                <div class="col-sm bg-success mt-4">
-                  <div class="col-sm">
-                    <button
-                      style={{ border: "none", backgroundColor: "transparent" }}
-                    >
-                      <i class="p-3 text-light rounded-circle  fas fa-check-circle fa-3x"></i>
-                    </button>
-                  </div>
-                </div>
-                <div class="col-sm bg-success">
-                  <div class="col-sm">
-                    <button
-                      style={{ border: "none", backgroundColor: "transparent" }}
-                    >
-                      <i class="p-3 text-light rounded-circle  fas fa-times-circle fa-3x"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div style={{ padding: "30px 350px", backgroundColor: "#FF723F" }}>
+              <h4 className='text-center' style={{ color: "#FFF" }}>Ventana Administración</h4>
+
+              <CardGroup>
+
+                  <Card className="m-5 text-center" >
+                      <Link to='/listaMarcas'>
+                          <Card.Body>
+                              <i class="far fa-copyright fa-5x" style={{ color: "#C42709" }}></i>
+                              <Card.Title >Marcas</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+
+                  <Card className="m-5 text-center">
+                      <Link to='/catalogoProductos'>
+                          <Card.Body>
+                              <i class="fab fa-product-hunt fa-5x " style={{ color: "#C42709" }}></i>
+                              <Card.Title>Productos</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+                  
+              </CardGroup>
+
+         
+              <CardGroup >
+
+              <Card className="m-5 text-center ">
+                      <Link to='/Proveedores'>
+                          <Card.Body>
+                              <i class="fas fa-people-carry fa-5x" style={{ color: "#C42709" }}></i>
+                              <Card.Title>Proveedores</Card.Title>
+                          </Card.Body>
+                      </Link>
+                  </Card>
+
+                  
+              </CardGroup>
           </div>
+      </div>
         );
 };
 
